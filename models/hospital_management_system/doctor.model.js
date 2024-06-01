c074a2e8c919dbf42a9a4396407154e981100b35
abdulrafayKhan-10 <smokeark3@gmail.com>
2024-06-02 03:12:34 +0500

    Commited

--- models/hospital_management_system/doctor.model.js
+++ models/hospital_management_system/doctor.model.js
@@ -1,5 +1,27 @@
 import mongoose from "mongoose";
 const doctorSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    salary:{
+        type: String,
+        required: true
+    },
+    qualification:{
+        type: String,
+        required: true
+    },
+    Experienced:{
+        type: String,
+        required: true
+    },
+    WorkInHospital: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "hospital"
+        }
+    ]
 
 },{
     timestamps: true
