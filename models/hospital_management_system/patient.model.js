f63ed44c99465f52550b1c929e6143e9f1baed07
abdulrafayKhan-10 <smokeark3@gmail.com>
2024-06-02 02:59:37 +0500

    Commtied

--- models/hospital_management_system/patient.model.js
+++ models/hospital_management_system/patient.model.js
@@ -1,6 +1,35 @@
 import mongoose from "mongoose";
 const patientSchema = new mongoose.Schema({
-
+ name:{
+    type: String, 
+    required: true
+ },
+ Diagonsedwith:{
+    type: String, 
+    required: true
+ },
+ Address:{
+    type: String, 
+    required: true
+ },
+ age:{
+    type: Number, 
+    required: true
+ },
+ BloodGroup:{
+    type: String, 
+    enum: ["A+","A-","B-","B+","O+","O-","AB+","AB-"],
+    required: true
+ },
+ Gender:{
+    type: String,
+    enum: ["M","F","O"],
+    required: true
+ },
+ AdmittedIn:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"doctor"
+ }
 },{
     timestamps: true
 });
