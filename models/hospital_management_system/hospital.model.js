193e3315a04455af288972cf09971447b10f40ef
abdulrafayKhan-10 <smokeark3@gmail.com>
2024-06-02 02:48:30 +0500

    Commited

---
+++ models/hospital_management_system/hospital.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const hospitalSchema = new mongoose.Schema({
+
+},{
+    timestamps: true
+});
+export const hospital = mongoose.model("hospital", hospitalSchema);
+
+
+
+
+
+
+
+
+
+
---
+++ models/hospital_management_system/medical_record.model.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+const medicalrecordSchema = new mongoose.Schema({
+
+},{
+    timestamps: true
+});
+export const medical_record = mongoose.model("medical_record", medicalrecordSchema);
