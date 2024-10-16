4ee38cd99b0e353487b401a3d519bd22ded33b8b
Abdul Rafay Khan <rafaykhan0000@gmail.com>
2024-10-16 21:35:46 +0500

    Updated

--- models/hospital_management_system/medical_record.model.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-const medicalrecordSchema = new mongoose.Schema({
-    
-},{
-    timestamps: true
-});
-export const medical_record = mongoose.model("medical_record", medicalrecordSchema);
